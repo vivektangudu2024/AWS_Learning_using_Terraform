6aa36a13acc0e85b2ee861363435457890d4e62a
vivektangudu2024 <75472985+vivektangudu2024@users.noreply.github.com>
2023-05-25 16:44:15 +0530

    added life cycle policies to the versioned bucket

--- S3/main.tf
+++ S3/main.tf
@@ -68,3 +68,34 @@ resource "aws_s3_bucket_acl" "test_bucket_acl" {
     }
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.test_bucket]
+
+  bucket = aws_s3_bucket.test_bucket.id
+
+  rule {
+    id = "config"
+
+    filter {
+      prefix = "config/"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+
+    status = "Enabled"
+  }
+}
