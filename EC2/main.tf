1bf93a587d0922829a2edffc8b73e07213cfbbeb
vivektangudu2024 <75472985+vivektangudu2024@users.noreply.github.com>
2023-05-30 16:35:37 +0530

    Update main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.67.0"
     }
   }
 
@@ -37,11 +37,12 @@ resource "aws_security_group" "example_sg" {
 
   
 }
-
+/*
 resource "aws_key_pair" "example_key_pair" {
   key_name   = "ExampleKeyPair"
   public_key = "ssh-rsa MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAnFKAxSxm7GfkesxUB0icSYbmIRzI2fiNM3KBv8dkJ3IP+rLSOwB/ZwXorMqJNA/4Z5NUsxr1meW1nj4zNf/l6pUrM8Ceb7ltREAWyHqVkOp4Tv6OIcgdaaWORqz2ia6ihdC2+tSXylIcL3T/GRzgaR+ljguoplgmrkWCrVT+e6LGyiZZWPYBJcEfhDkIT619raZ8TuoVaTUxbsk/D9W2sM+5RyXWX6VugHPJ0eEHVScM2HmzeUyqvlxE/NqUm2H4xLkGWqfL3wt4ObGuBEt537eyyKKg7jgMxVYaMvA+FOrU8YePzRrmMzORJZJZxZWEpz/KrAdX+GphZHDiL3FsahJci0guquCcp6irf+sx1v/lUttk55r7GjyGyad4V3HhMbKikd/CPuLLYWVLD12mGIbSAlOB6/io7bbI45iPXmhg41STY5+9dAM58gD96oHz3uUdWapRZYeH4tngv4Dx9knWnUbcuiy8Y9kceWEdVO+bMKqhPaglyAdVvolL3U7a4/wWCxCy4dQNF5Rh5W/YbzcHxXmg5KpTtw4TNBHw46PG00bWmvVcjnV3lj/t/9tSPKQyg+q4qwI+Vont004lLTYCMiB8pX2Fmq6FV4k3rOdGwsCR+ppI+mIzUq2b5NnECc1Dd1UyO1/vu14LVi9NiEuneNUO5Ncyh9dv4hOrmdcCAwEAAQ=="
 }
+*/
 
 resource "aws_autoscaling_group" "example_asg" {
   name                      = "ExampleAutoScalingGroup"
@@ -50,7 +51,7 @@ resource "aws_autoscaling_group" "example_asg" {
   desired_capacity          = 2
  
   launch_configuration      = aws_launch_configuration.example_lc.name
-
+  availability_zones = ["us-west-2a"]
   
 }
 
@@ -58,14 +59,14 @@ resource "aws_launch_configuration" "example_lc" {
   name          = "ExampleLaunchConfiguration"
   image_id      = "ami-08d70e59c07c61a3a"  
   instance_type = "t2.micro"  
-
+  
   
 }
 
 resource "aws_elb" "example_elb" {
   name               = "ExampleLoadBalancer"
   security_groups    = [aws_security_group.example_sg.id]
-  availability_zones = ["us-east-1a", "us-east-1b"]  # Replace with desired availability zones
+  availability_zones = ["us-west-2a", "us-west-2b"]  # Replace with desired availability zones
 
   listener {
     instance_port     = 80
@@ -73,13 +74,7 @@ resource "aws_elb" "example_elb" {
     lb_port           = 80
     lb_protocol       = "HTTP"
   }
-  listener {
-    instance_port      = 80
-    instance_protocol  = "HTTP"
-    lb_port            = 443
-    lb_protocol        = "HTTPS"
-    
-  }
+  
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
