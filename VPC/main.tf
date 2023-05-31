003e366e4e4e491da11a3ee319a4aba22b69715e
Shantanuban09 <shantanuban09@gmail.com>
2023-05-31 16:36:16 +0530

    Added VPC Peeering and EC2 instances in different subnets

--- VPC/main.tf
+++ VPC/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 # Create VPC
@@ -25,7 +25,7 @@ resource "aws_vpc" "example_vpc" {
 resource "aws_subnet" "example_subnet1" {
   vpc_id                  = aws_vpc.example_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "ExampleSubnet1"
@@ -35,7 +35,7 @@ resource "aws_subnet" "example_subnet1" {
 resource "aws_subnet" "example_subnet2" {
   vpc_id                  = aws_vpc.example_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
     Name = "ExampleSubnet2"
@@ -85,6 +85,79 @@ resource "aws_route_table_association" "example_subnet1_association" {
 resource "aws_route_table_association" "example_subnet2_association" {
   subnet_id      = aws_subnet.example_subnet2.id
   route_table_id = aws_route_table.example_rt.id
+}
+
+resource "aws_instance" "instance_1_subnet_1" {
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.example_subnet1.id
+
+  tags = {
+    Name = " Subnet 1 I1"
+  }
+}
+resource "aws_instance" "instance_2_subnet_1" {
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.example_subnet1.id
+
+  tags = {
+    Name = " Subnet 1 I2"
+  }
+}
+
+resource "aws_instance" "instance_1_subnet_2" {
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.example_subnet2.id
+
+  tags = {
+    Name = " Subnet 2 I1"
+  }
+}
+
+resource "aws_instance" "instance_2_subnet_2" {
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.example_subnet2.id
+
+  tags = {
+    Name = " Subnet 2 I2"
+  }
+}
+
+// Creating another VPC
+resource "aws_vpc" "example_vpc2" {
+  cidr_block = "192.168.0.0/24"
+  instance_tenancy = "default"
+  tags = {
+    Name = "Example VPC 2"
+  }
+}
+
+resource "aws_subnet" "example2_subnet1" {
+  vpc_id = aws_vpc.example_vpc2.id
+  cidr_block = "192.168.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Example2 Subnet 1"
+  }
+}
+
+//Peering
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  vpc_id        = aws_vpc.example_vpc.id
+  peer_vpc_id   = aws_vpc.example_vpc2.id
+  auto_accept   = true
+}
+
+// Route to peered VPC
+resource "aws_route" "peer_route" {
+    route_table_id = aws_route_table.example_rt.id
+    destination_cidr_block = aws_vpc.example_vpc2.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
 }
 
 # Create network ACL
